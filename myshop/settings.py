d88c6c477cb05270777b93a517f85ff63a0a6da2
mielsen35 <wremennyadress@gmail.com>
2024-07-01 11:17:08 +0300

    Вроде домучался но оплата не пашет но ничо скачаем с репозитария книги

--- myshop/settings.py
+++ myshop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'coupons.apps.CouponsConfig',
     'rosetta',
     'parler',
+    'localflavor',
 ]
 
 MIDDLEWARE = [
--- orders/forms.py
+++ orders/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+from localflavor.us.forms import USZipCodeField
 from .models import Order
+
 
 
 class OrderCreateForm(forms.ModelForm):
     class Meta:
+        postal_code = USZipCodeField()
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address',
                   'postal_code', 'city']
